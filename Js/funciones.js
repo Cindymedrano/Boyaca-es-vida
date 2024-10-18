9b52a56fc8d62ab318abf148b78a6067ab807439
Cindy Paola Medrano Lopez <cindypaolamedrano@gmail.com>
2024-10-18 01:47:14 -0500

    se hacen modificaciones a pagina de prueba

---
+++ Js/funciones.js
@@ -0,0 +1,11 @@
+function buscar(){
+    alert( "entro a  mi funcion");
+
+    let busqueda = document.getElementById("testId-SearchBar-Input").value;
+
+
+    if (busqueda == "" ) {
+        alert("el valor de busqueda no puede estar vacio")
+
+}
+}
