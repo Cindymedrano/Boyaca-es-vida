253cad53257d59925caa9ddeea93439c46016c94
Cindy Paola Medrano Lopez <cindypaolamedrano@gmail.com>
2024-10-21 09:38:07 -0500

    se agregan id a header

--- Js/funciones.js
+++ Js/funciones.js
@@ -32,3 +32,35 @@ function buscar() {
     }
 }
 
+
+
+/* //visualizarFooter();//se llama la funciòn al cargar el archivo
+function visualizarFooter(){
+    let seccionFooter= document.getElementById("footer");
+    seccionFooter.innerHTML= `
+     <div class="grupo-1">
+        <div class="box">
+            <figure>
+                    <img src="Imagenes/PaginaPrincipal/Logo.png" alt="logo boyaca es vida">   
+            </figure>
+        </div>
+        <div class="box">
+            <h2> SOBRE NOSOTROS</h2>
+            <p>Boyacá es vida nace de la admiración por la belleza natural, historia y rica cultura que posee éste
+                departamento. </p>
+            <p>Nuestra misión es que descubras la esencia de Boyacá con nosotros. </p>
+        </div>
+        <div class="box">
+            <h2> SIGUENOS</h2>
+            <div class="red-social">
+                <a href="https://www.instagram.com/boyacaesvida/" class="fa fa-instagram"  target="_blank"></a>
+                <a href="https://www.facebook.com/profile.php?id=61567079648836"class="fa fa-facebook"  target="_blank"></a>
+            </div>
+        </div>
+    </div>
+    <div class="grupo-2">
+        <small>&copy; 2024 <b>Boyacá es vida</b> - Todos los derechos reservados.</small>
+    </div>`;
+
+
+} */
